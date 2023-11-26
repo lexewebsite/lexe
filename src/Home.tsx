361165a84c643b51cc4da071b53d2341250be3f3
Chris Liu <59787860+acciochris@users.noreply.github.com>
2023-11-26 22:52:47 +0000

    Add stats

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,27 +1,42 @@
 function Home() {
   return (
-    <>
+    <div class="px-4 md:px-12 xl:px-48 flex flex-col items-center">
       <div>
         <h1 class="text-4xl font-extrabold text-center m-6">Fundraising Goals</h1>
-        <div class="grid grid-cols-1 md:grid-cols-2 items-center px-8 gap-6">
-          <div class="prose">
-            <p>
-              Our fundraising goal for this year is $300,000. So far, we are at 55% of our fundraising
-              goal with contributions from 25.4% of parents...imagine what we can do for Lynbrook with
-              100% parent participation!
-            </p>
-          </div>
-          <div class="flex flex-row place-content-evenly not-prose">
-            <div class="radial-progress text-primary-content text-center bg-primary border-4 border-primary" style="--value:25; --size:12rem; --thickness: 1rem;" role="progressbar">
-              <p class="text-xl font-bold">25%</p>parents<br />have contributed
-            </div>
-            <div class="radial-progress text-secondary-content text-center bg-secondary border-4 border-secondary" style="--value:55; --size:12rem; --thickness: 1rem;" role="progressbar">
-              <p class="text-xl font-bold">55%</p>fundraising<br />goal achieved
-            </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 items-center px-4 gap-6">
+          <p class="text-lg">
+            Our fundraising goal for this year is $300,000. So far, we are at 55% of our fundraising
+            goal with contributions from 25.4% of parents...imagine what we can do for Lynbrook with
+            100% parent participation!
+          </p>
+          <div class="flex flex-col gap-1">
+            <progress class="progress progress-primary w-full" value="25" max="100"></progress>
+            <p class="text-xl tracking-wider">25% parents have contributed</p>
+            <progress class="progress progress-secondary w-full" value="55" max="100"></progress>
+            <p class="text-xl tracking-wider ">55% fundraising goal achieved</p>
           </div>
         </div>
       </div>
-    </>
+
+      <div class="stats stats-vertical md:stats-horizontal md:my-12 my-4 w-full">
+        <div class="stat place-items-center">
+          <div class="stat-value xl:text-7xl">210</div>
+          <div class="stat-title xl:text-2xl">Projects Funded</div>
+        </div>
+        <div class="stat place-items-center">
+          <div class="stat-value xl:text-7xl">$1.5M</div>
+          <div class="stat-title xl:text-2xl">Grants Awarded</div>
+        </div>
+        <div class="stat place-items-center">
+          <div class="stat-value xl:text-7xl">14</div>
+          <div class="stat-title xl:text-2xl">Departments Supported</div>
+        </div>
+        <div class="stat place-items-center">
+          <div class="stat-value xl:text-7xl">4,100</div>
+          <div class="stat-title xl:text-2xl">Students Helped</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
