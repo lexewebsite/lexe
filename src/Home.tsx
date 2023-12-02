8b2553db41dce8c57994415d6da868386651eea6
Chris Liu <59787860+acciochris@users.noreply.github.com>
2023-12-02 02:32:12 +0000

    Add banner image

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,11 @@
+import banner from './assets/home-banner.jpeg'
+
 function Home() {
   return (
-    <div class="px-4 md:px-12 xl:px-48 flex flex-col items-center">
-      <div>
+    <div class="px-4 md:px-12 xl:px-48 py-12 flex flex-col items-center">
+      <img src={banner} loading="lazy" class="mb-2 w-full mx-6"/>
+
+      <div class="my-2">
         <h1 class="text-4xl font-extrabold text-center m-6">Fundraising Goals</h1>
         <div class="grid grid-cols-1 md:grid-cols-2 items-center px-4 gap-6">
           <p class="text-lg">
@@ -13,7 +17,7 @@ function Home() {
             <progress class="progress progress-primary w-full" value="25" max="100"></progress>
             <p class="text-xl tracking-wider">25% parents have contributed</p>
             <progress class="progress progress-secondary w-full" value="55" max="100"></progress>
-            <p class="text-xl tracking-wider ">55% fundraising goal achieved</p>
+            <p class="text-xl tracking-wider">55% fundraising goal achieved</p>
           </div>
         </div>
       </div>
@@ -40,7 +44,7 @@ function Home() {
       <iframe
         src="https://www.youtube-nocookie.com/embed/-3a7U0o84ZQ?si=yOiKCg8Ij4C3OW1C" title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        class="w-full max-w-xl h-96 my-2"
+        class="w-full max-w-xl h-72 sm:h-96 mt-2"
         loading="lazy"
         allowfullscreen
       ></iframe>
