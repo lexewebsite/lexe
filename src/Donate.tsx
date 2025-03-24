f12e0a70d38850b1e46d761a00db2ca9cc922a07
Chris Liu <59787860+acciochris@users.noreply.github.com>
2025-03-24 05:01:00 +0000

    Polish Donate page

--- src/Donate.tsx
+++ src/Donate.tsx
@@ -5,45 +5,36 @@ function Donate() {
   const paragraph =
     "The fundraising goal for the current school year is $300,000. The suggested contribution per family is $ 425 or whatever fits your budget. Lynbrook Excellence in Education is an IRS approved 501(c)(3) public charity. Based on this status all contributions are tax-deductible. We will gladly accept contributions of cash, check, or by credit card.";
   const DONATION_STRING =
-    "https://www.paypal.com/donate?token=tDEyf_n4cjKFXc6bnSc1OBmdIYrvBGRAu2zy2ldIz-Dm25MZDOqMsC6ECRNVxz2vIKgcRt3qJgf87C2M";
+    "https://www.paypal.com/donate?token=_1Lnq5WletsSAbO4Q-pZHLiIDOzsRUHIV4UoVF_b6yHwUgfhqgrvPL8L1uWVVkmWKYK8NfA8WrmcAae0";
   return (
-    <div class="w-full text-center">
-      <h1 class="text-7xl text-center m-12 w-full">Donate</h1>
-      <div class="w-full grid grid-cols-4">
-        <span class="col-span-1"></span>
-        <p class="text-left w-full col-span-2"> {paragraph} </p>
-      </div>
+    <div class="container px-4 py-12 flex flex-col items-center gap-2 text-base-content">
+      <h1 class="text-5xl font-extrabold text-center mb-4">Donate</h1>
+      <p class="text-left text-pretty text-md lg:text-xl max-w-4xl"> {paragraph} </p>
 
       {/** This section of the website should go horizontally down while be text-lefted.*/}
-      <div class="w-full flex flex-col gap-2 mt-4 ml-[25%] mb-[15%]">
-        <a href={DONATION_STRING} target="_blank">
-          {" "}
+      <div class="container flex flex-col gap-2 max-w-4xl">
+        <a href={DONATION_STRING} target="_blank" class="w-64 self-center">
           <img
             src={donation}
-            width={150}
+            class="w-full"
             alt="Please click on this image in order to access our page for donations."
-          />{" "}
+          />
         </a>
-        <p class="text-2xl text-left">
-          {" "}
-          <strong> By Mail: </strong>{" "}
+        <p class="text-2xl lg:text-3xl mt-8 font-extrabold">
+          By Mail:
         </p>
-        <p class="text-md text-left text-gray-400">
-          {" "}
+        <p class="text-md lg:text-xl max-w-4xl">
           Download the{" "}
-          <a href={PDF} class="text-purple-500" target="_blank">
-            {" "}
-            Donation Form{" "}
+          <a href={PDF} class="text-primary" target="_blank">
+            Donation Form
           </a>{" "}
-          and mail with your donation to:{" "}
+          and mail with your donation to:
         </p>
-        <p class="text-2xl text-left">
-          {" "}
-          <strong> Our Address: </strong>{" "}
+        <p class="text-2xl lg:text-3xl mt-4 font-extrabold">
+          Our Address:
         </p>
-        <p class="text-md text-left text-gray-400">
-          {" "}
-          1280 Johnson Ave, San Jose, CA 95129.{" "}
+        <p class="text-md lg:text-xl max-w-4xl">
+          1280 Johnson Ave, San Jose, CA 95129.
         </p>
       </div>
     </div>
