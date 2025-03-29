fde6a65e958977dbe1dca9aea537cf4d9f6789ec
Chris Liu <59787860+acciochris@users.noreply.github.com>
2025-03-29 04:13:40 +0000

    Add updates page

---
+++ src/Updates.tsx
@@ -0,0 +1,17 @@
+function Updates() {
+  return (
+    <div class="container px-4 py-12 flex flex-col items-center gap-2 text-base-content">
+      <h1 class="text-5xl font-extrabold text-center mb-4">Updates</h1>
+      <div class="container flex flex-col gap-2 max-w-4xl">
+        <h2 class="text-2xl lg:text-3xl mt-8 font-extrabold" id="newsletters">
+          Newsletters
+        </h2>
+        <h2 class="text-2xl lg:text-3xl mt-8 font-extrabold" id="annual-reports">
+          Annual Reports
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export default Updates;
