6346f91a50fa1fbd986d4919ff3e4b073782b2cf
Chris Liu <59787860+acciochris@users.noreply.github.com>
2025-04-06 22:55:53 +0000

    Add annual reports and newsletters from google drive

--- src/Updates.tsx
+++ src/Updates.tsx
@@ -1,4 +1,15 @@
-import report2020 from "./assets/reports/annualReport2020.pdf";
+const annual_reports = {
+  "2017-2018": "https://drive.google.com/file/d/0Bx6shaV-C7LnTnVqMGZaMnQzeTVOTUQ2cUpKMThWd1BSZUFR/view?usp=drive_link&resourcekey=0-Lnopz-L4ZxkRoKL4SNbwwA",
+  "2019-2020": "https://drive.google.com/file/d/1hRUILwnsIQ9epGcDL3EQoYI6ZgUI7LiN/view?usp=drive_link",
+  "2020-2021": "https://drive.google.com/file/d/10SsxtgWu47dIMjqR3tuwMlQxvnp_bKTN/view?usp=drive_link",
+  "2021-2022": "https://drive.google.com/file/d/187_oU6inPWWfMalCjRNDz48jjpz9s66l/view?usp=drive_link",
+  "2022-2023": "https://drive.google.com/file/d/1Cb6WhIVtPQKgYDONz8eKaJ9VVRH1_Cii/view?usp=drive_link",
+};
+
+const newsletters = {
+  "2020": "https://app.flashissue.com/newsletters/44bf3813e1bc67cb1bbee671c9d7f517b05e9c51",
+  "Nov 2024": "https://docs.google.com/document/d/1bPBqb1ZfGBwCjaErTuoMIQ_3lWkEgtY3RR_ufeT_DS0/edit?usp=sharing",
+};
 
 function Updates() {
   return (
@@ -8,14 +19,20 @@ function Updates() {
         <h2 class="text-2xl lg:text-3xl mt-8 font-extrabold" id="newsletters">
           Newsletters
         </h2>
-        <div class="grid grid-cols-1 md:grid-cols-4 mt-1">
-          <a
-            class="btn btn-accent text-lg font-normal"
-            target="_blank"
-            href="https://app.flashissue.com/newsletters/44bf3813e1bc67cb1bbee671c9d7f517b05e9c51"
-          >
-            2020
-          </a>
+        <div class="grid grid-cols-1 md:grid-cols-4 mt-1 gap-2">
+          {
+            Object.keys(newsletters).map(key => {
+              return (
+                <a
+                  class="btn btn-accent text-lg font-normal"
+                  target="_blank"
+                  href={newsletters[key]}
+                >
+                  {key}
+                </a>
+              )
+            })
+          }
         </div>
 
         <h2
@@ -24,14 +41,20 @@ function Updates() {
         >
           Annual Reports
         </h2>
-        <div class="grid grid-cols-1 md:grid-cols-4 mt-1">
-          <a
-            class="btn btn-accent text-lg font-normal"
-            target="_blank"
-            href={report2020}
-          >
-            2020
-          </a>
+        <div class="grid grid-cols-1 md:grid-cols-4 mt-1 gap-2">
+          {
+            Object.keys(annual_reports).map(key => {
+              return (
+                <a
+                  class="btn btn-accent text-lg font-normal"
+                  target="_blank"
+                  href={annual_reports[key]}
+                >
+                  {key}
+                </a>
+              )
+            })
+          }
         </div>
       </div>
     </div>
