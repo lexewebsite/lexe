9714d0d5c0119a687df3117f110073aef0d6e94e
Chris Liu <59787860+acciochris@users.noreply.github.com>
2025-04-06 21:40:43 +0000

    Add 2020 newsletter and report

--- src/Updates.tsx
+++ src/Updates.tsx
@@ -1,3 +1,5 @@
+import report2020 from "./assets/reports/annualReport2020.pdf";
+
 function Updates() {
   return (
     <div class="container px-4 py-12 flex flex-col items-center gap-2 text-base-content">
@@ -6,9 +8,16 @@ function Updates() {
         <h2 class="text-2xl lg:text-3xl mt-8 font-extrabold" id="newsletters">
           Newsletters
         </h2>
+        <div class="grid grid-cols-1 md:grid-cols-4 mt-1">
+          <a class="btn btn-accent text-lg font-normal" href="https://app.flashissue.com/newsletters/44bf3813e1bc67cb1bbee671c9d7f517b05e9c51">2020</a>
+        </div>
+
         <h2 class="text-2xl lg:text-3xl mt-8 font-extrabold" id="annual-reports">
           Annual Reports
         </h2>
+        <div class="grid grid-cols-1 md:grid-cols-4 mt-1">
+          <a class="btn btn-accent text-lg font-normal" href={report2020}>2020</a>
+        </div>
       </div>
     </div>
   )
